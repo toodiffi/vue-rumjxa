d82d3b6fb336022790b173926638e7bd62cab626
toodiffi <135706449+toodiffi@users.noreply.github.com>
2023-06-06 19:49:08 +0800

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <!-- <img alt="Vue logo" src="https://vuejs.org/images/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
